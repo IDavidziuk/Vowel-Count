9c4d8b41f5b7e39c8a4666a8bddd7a0aeab52839
Ina-Davidziuk <ina.davidziuk@gmail.com>
2023-07-08 23:58:03 +0200

    comlete task

--- Lazy_Repeater.js
+++ Lazy_Repeater.js
@@ -20,3 +20,15 @@ function squaresNeeded(grains){
     //your code here
     return Math.ceil(Math.log2(grains+1))
   }
+
+//Replicate `new`
+function nouveau (Constructor, ...args) {
+    // Don't forget, unnamed arguments after Constructor may be passed in!
+    const instance = Object.create(Constructor.prototype);
+    const callFunction = Constructor.call(instance, ...args);
+          if((typeof callFunction === 'object' || typeof callFunction === 'function') && callFunction !== null) {
+            return callFunction;
+          } else {
+            return instance;
+          }
+  }
